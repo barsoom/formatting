2ce5224250e8ce5a0f39afbf645b7a1d0d852aed
Henrik Nyh <henrik@nyh.se>
2013-10-07 11:06:37 +0200

    Rounding.

--- lib/formatting.rb
+++ lib/formatting.rb
@@ -5,11 +5,17 @@ module Formatting
   THOUSANDS_SEPARATOR = NON_BREAKING_SPACE
   DECIMAL_SEPARATOR = "."
 
-  def self.format_number(number)
+  def self.format_number(number, opts = {})
     thousands_separator = THOUSANDS_SEPARATOR
     decimal_separator = DECIMAL_SEPARATOR
+    round = opts.fetch(:round, nil)
+
+    if round
+      number = number.round(round)
+    end
 
     integer, decimals = number.to_s.split(".")
+
     integer.gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{thousands_separator}")
     [integer, decimals].join(decimal_separator)
   end
--- spec/formatting_spec.rb
+++ spec/formatting_spec.rb
@@ -2,12 +2,22 @@ require "formatting"
 
 describe Formatting do
   describe ".format_number" do
-    it "formats" do
+    it "groups thousands" do
       expect_formatted(1234567.89).to eq space_to_nbsp("1 234 567.89")
     end
 
-    def expect_formatted(value)
-      expect(Formatting.format_number(value))
+    context "rounding" do
+      it "doesn't round by default" do
+        expect_formatted(12.3456789).to eq "12.3456789"
+      end
+
+      it "rounds to the given number of decimals" do
+        expect_formatted(12.3456789, round: 2).to eq "12.35"
+      end
+    end
+
+    def expect_formatted(value, opts = {})
+      expect(Formatting.format_number(value, opts))
     end
 
     def space_to_nbsp(value)
