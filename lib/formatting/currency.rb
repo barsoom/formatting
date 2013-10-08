0bfb44e13427f86c643342573c2862b8c9cec8ae
Henrik Nyh <henrik@nyh.se>
2013-10-08 09:50:39 +0200

    format_currency takes record + amount

--- lib/formatting/currency.rb
+++ lib/formatting/currency.rb
@@ -2,19 +2,21 @@ module Formatting
   module Currency
     include Number
 
-    def format_currency(number, opts = {})
+    def format_currency(record, amount_or_method, opts = {})
       currency      = opts.fetch(:currency, nil)
-      format_string = opts.fetch(:format, "<number> <currency>")
+      format_string = opts.fetch(:format, "<amount> <currency>")
 
-      number = format_number(number, opts)
-      apply_format_string(format_string, number, currency)
+      amount = amount_or_method
+
+      amount = format_number(amount, opts)
+      apply_format_string(format_string, amount, currency)
     end
 
     private
 
-    def apply_format_string(format_string, number, currency)
+    def apply_format_string(format_string, amount, currency)
       out = format_string.dup
-      out.gsub!("<number>", number)
+      out.gsub!("<amount>", amount)
       out.gsub!("<currency>", currency.to_s)
       out.strip!
       out.gsub!(" ", NON_BREAKING_SPACE)
--- spec/currency_spec.rb
+++ spec/currency_spec.rb
@@ -10,44 +10,46 @@ end
 
 describe Formatting do
   describe ".format_currency" do
+    let(:item) { double(price: 123.45) }
+
     it "formats numbers" do
-      expect_formatted(1234.56).to include space_to_nbsp("1 234.56")
+      expect_formatted(item, 1234.56).to include space_to_nbsp("1 234.56")
     end
 
     it "passes on number formatting options" do
-      expect_formatted(1234.567, round: 2).to include space_to_nbsp("1 234.57")
+      expect_formatted(item, 1234.567, round: 2).to include space_to_nbsp("1 234.57")
     end
 
     context "currency option" do
       it "is added if provided" do
-        expect_formatted(1234.56, currency: "XYZ").to eq space_to_nbsp("1 234.56 XYZ")
+        expect_formatted(item, 1234.56, currency: "XYZ").to eq space_to_nbsp("1 234.56 XYZ")
       end
 
       it "may be nil" do
-        expect_formatted(1234.56).to eq space_to_nbsp("1 234.56")
+        expect_formatted(item, 1234.56).to eq space_to_nbsp("1 234.56")
       end
     end
 
     context "format string option" do
       it "is used if provided" do
-        expect_formatted(123, format: "C: <currency> N: <number>", currency: "XYZ").
-          to eq space_to_nbsp("C: XYZ N: 123.0")
+        expect_formatted(item, 123, format: "C: <currency> A: <amount>", currency: "XYZ").
+          to eq space_to_nbsp("C: XYZ A: 123.0")
       end
 
       it "will have spaces turned into non-breaking spaces" do
-        expect_actual = expect_formatted(123, format: "<number> <currency>", currency: "XYZ")
+        expect_actual = expect_formatted(item, 123, format: "<amount> <currency>", currency: "XYZ")
         expect_actual.to eq space_to_nbsp("123.0 XYZ")
         expect_actual.not_to eq "123.0 XYZ"
       end
 
       it "is stripped" do
-        expect_formatted(123, format: "<number> <currency>", currency: nil).
+        expect_formatted(item, 123, format: "<amount> <currency>", currency: nil).
           to eq space_to_nbsp("123.0")
       end
     end
   end
 
-  def expect_formatted(value, opts = {})
-    expect(Formatting.format_currency(value, opts))
+  def expect_formatted(record, value, opts = {})
+    expect(Formatting.format_currency(record, value, opts))
   end
 end
