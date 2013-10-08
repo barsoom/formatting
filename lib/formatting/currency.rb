385731282b5947386c457bd1eb6342816ec44222
Henrik Nyh <henrik@nyh.se>
2013-10-08 09:42:58 +0200

    More currency.

--- lib/formatting/currency.rb
+++ lib/formatting/currency.rb
@@ -3,7 +3,17 @@ module Formatting
     include Number
 
     def format_currency(number, opts = {})
-      format_number(number, opts)
+      currency = opts.fetch(:currency, nil)
+      fstring  = opts.fetch(:format, "<number> <currency>")
+
+      number = format_number(number, opts)
+
+      out = fstring.dup
+      out.gsub!("<number>", number)
+      out.gsub!("<currency>", currency.to_s)
+      out.strip!
+      out.gsub!(" ", NON_BREAKING_SPACE)
+      out
     end
   end
 end
--- spec/currency_spec.rb
+++ spec/currency_spec.rb
@@ -11,11 +11,38 @@ end
 describe Formatting do
   describe ".format_currency" do
     it "formats numbers" do
-      expect_formatted(1234.567).to eq space_to_nbsp("1 234.567")
+      expect_formatted(1234.56).to include space_to_nbsp("1 234.56")
     end
 
     it "passes on number formatting options" do
-      expect_formatted(1234.567, round: 2).to eq space_to_nbsp("1 234.57")
+      expect_formatted(1234.567, round: 2).to include space_to_nbsp("1 234.57")
+    end
+
+    context "currency" do
+      it "is added if provided" do
+        expect_formatted(1234.56, currency: "XYZ").to eq space_to_nbsp("1 234.56 XYZ")
+      end
+
+      it "may be nil" do
+        expect_formatted(1234.56).to eq space_to_nbsp("1 234.56")
+      end
+    end
+
+    context "custom format string" do
+      it "can be provided" do
+        expect_formatted(123, format: "C: <currency> N: <number>", currency: "XYZ").
+          to eq space_to_nbsp("C: XYZ N: 123.0")
+      end
+
+      it "replaces spaces with non-breaking spaces" do
+        expect_formatted(123, currency: "XYZ").to eq space_to_nbsp("123.0 XYZ")
+        expect_formatted(123, currency: "XYZ").not_to eq "123.0 XYZ"
+      end
+
+      it "is stripped" do
+        expect_formatted(123, format: "<number> <currency>", currency: nil).
+          to eq space_to_nbsp("123.0")
+      end
     end
   end
 
