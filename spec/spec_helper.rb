74f89424e0db235a0b0a1c9dbb819dc27330d51d
Olle Jonsson <olle.jonsson@auctionet.com>
2021-07-26 13:08:15 +0200

    RSpec: persist results, disable monkey patching...
    ...and randomize.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,10 @@ end
 
 RSpec.configure do |config|
   config.include Helpers
+
+  config.example_status_persistence_file_path = ".rspec_results"
+  config.disable_monkey_patching!
+
+  config.order = :random
+  Kernel.srand config.seed
 end
